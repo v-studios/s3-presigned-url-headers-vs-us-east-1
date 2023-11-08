31a5ed0502e9aee5a7c179f6728499d3cac2e497
Chris Shenton <chris@v-studios.com>
2023-11-08 11:47:47 +0100

    The URL-generator returns HTTP headers that the PUT must include

--- psurl.py
+++ psurl.py
@@ -18,6 +18,12 @@ def get_psurl(region, bucket, filename):
     mimetype = mimetypes.guess_type(filename)[0]
 
     s3c = boto3.client("s3", region_name=region)  # region overrides AWS_PROFILE
+    metadata = {
+        # If I put anything in Metadata, upload fails in all but us-east-1.
+        # To fix, the PUT must include these headers with x-amz-meta-KEY: VALUE
+        "filename": filename,
+        "Content-Disposition": f"attachment; filename={filename}",
+    }
     url = s3c.generate_presigned_url(
         ClientMethod="put_object",
         ExpiresIn=3600,
@@ -26,34 +32,24 @@ def get_psurl(region, bucket, filename):
             "Bucket": bucket,
             "Key": filename,
             "ContentType": mimetype,
-            "Metadata": {
-                # If I put anything in Metadata, upload fails in all but us-east-1.
-                # To fix, the PUT must include these headers with x-amz-meta-KEY: VALUE
-                "filename": filename,
-                "Content-Disposition": f"attachment; filename={filename}",
-            },
+            "Metadata": metadata,
         }
     )
+    headers = {f"x-amz-meta-{k.lower()}": v for k, v in metadata.items()}
+    headers.update({"Content-Type": mimetype})
     return {
         "Bucket": bucket,
         "Filename": filename,
-        "Content-Type": mimetype,
+        "Headers": headers,
         "URL": url,
     }
 
 
-def put_file(filename, content_type, put_url):
-    print(f"Uploading {filename=} {content_type=} {put_url[:99]}")
+def put_file(filename, headers, put_url):
+    print(f"Uploading {filename=} {headers=} {put_url[:99]}")
     with open(filename, "rb") as upload_file:
         content = upload_file.read()
-        headers = {
-            # Must set the Content-Type because the PSURL includes it.
-            "Content-Type": content_type,
-            # For regions other than us-east-1 (!)
-            # you MUST be x-amz-meta headers to match Metadata in PSURL:
-            "x-amz-meta-filename": filename,
-            "x-amz-meta-content-disposition": f"attachment; filename={filename}",
-        }
+        headers = headers
         res = requests.put(put_url, data=content, headers=headers)
         return res
 
@@ -65,17 +61,17 @@ if __name__ == "__main__":
     To prove the PSURL works, we remvoe the AWS_PROFILE when uploading.
     """
     for region, bucket in REGION_BUCKETS.items():
+        filename = "fire.png"
         bucket = REGION_BUCKETS[region]
-        res = get_psurl(region=region, bucket=bucket, filename="fire.png")
+        res = get_psurl(region=region, bucket=bucket, filename=filename)
 
-        content_type = res["Content-Type"]
         put_url = res["URL"]
-        filename = res["Filename"]
-        print(f"{put_url[:99]=}")
+        headers = res["Headers"]
+        print(f"{headers=} {put_url[:99]=}")
 
         aws_profile = os.environ["AWS_PROFILE"]
         del os.environ["AWS_PROFILE"]
-        res = put_file(filename, content_type, put_url)
+        res = put_file(filename=filename, headers=headers, put_url=put_url)
         print(f"{res.status_code=} {res.reason=}")
         if res.status_code != 200:
             print(f"ERROR {res.content[:90]}")
