f7c8aec29fca4d4b264b4a0bd87c743db823b300
Chris Shenton <chris@v-studios.com>
2023-11-09 16:18:24 +0100

    Working: merge more of desired; TODO x-amz-meta-content-disposition

--- psurl.py
+++ psurl.py
@@ -8,45 +8,50 @@ import requests
 REGION_BUCKETS = {
     "us-east-1": "psurl-dev-s3assets-111savi37w6pt",
     "us-east-2": "psurl-dev-s3assets-1btlz2jfl73sj",
-    "eu-west-3": "psurl-dev-s3assets-19rz00qdke5v6",
+    #"eu-west-3": "psurl-dev-s3assets-19rz00qdke5v6",
 }
 
 
 def get_psurl(region, bucket, filename):
-    print(f"get_psurl: {region=} {bucket=} {filename=}")
+    """Generate a presigned URL to PUT a file to S3 bucket in a given region.
 
+    Return the URL, HTTP method, and headers the uploader will need to send.
+    """
+    content_disposition = f'attachment; filename="{filename}"'
+    http_method = "PUT"
     mimetype = mimetypes.guess_type(filename)[0]
-
-    s3c = boto3.client("s3", region_name=region)  # region overrides AWS_PROFILE
+    # If I put anything in Metadata, upload fails in regions *except* us-east-1;
+    # to fix, the HTTP PUT must include these headers with x-amz-meta-KEY: VALUE
     metadata = {
-        # If I put anything in Metadata, upload fails in all but us-east-1.
-        # To fix, the PUT must include these headers with x-amz-meta-KEY: VALUE
         "filename": filename,
-        "Content-Disposition": f"attachment; filename={filename}",
+        "magic_words": "Squeamish Ossifrage",
+        "Content-Disposition": content_disposition,
     }
+    s3c = boto3.client("s3", region_name=region)  # region overrides AWS_PROFILE
     url = s3c.generate_presigned_url(
         ClientMethod="put_object",
         ExpiresIn=3600,
-        HttpMethod="PUT",
+        HttpMethod=http_method,
         Params={
             "Bucket": bucket,
             "Key": filename,
+            # These params must be matched in the PUT by HTTP-named headers
             "ContentType": mimetype,
+            # Metadata items must be matched in the PUT by HTTP headers with prefix x-amz-meta-
             "Metadata": metadata,
         }
     )
     headers = {f"x-amz-meta-{k.lower()}": v for k, v in metadata.items()}
+    # If we change the Parameters above we'll need to update these:
     headers.update({"Content-Type": mimetype})
     return {
-        "Bucket": bucket,
-        "Filename": filename,
         "Headers": headers,
+        "Method": http_method,
         "URL": url,
     }
 
 
 def put_file(filename, headers, put_url):
-    #print(f"Uploading {filename=} {headers=} {put_url[:99]}")
     with open(filename, "rb") as upload_file:
         content = upload_file.read()
         headers = headers
@@ -65,16 +70,16 @@ if __name__ == "__main__":
         bucket = REGION_BUCKETS[region]
         res = get_psurl(region=region, bucket=bucket, filename=filename)
 
-        put_url = res["URL"]
+        method = res["Method"]
         headers = res["Headers"]
-        print(f"{headers=}")
-        print(f"{put_url[:99]=}")
+        put_url = res["URL"]
+        print(f"{region=} {method=}\n{headers=}\n{put_url[:99]=}")
 
         aws_profile = os.environ["AWS_PROFILE"]
         del os.environ["AWS_PROFILE"]
         res = put_file(filename=filename, headers=headers, put_url=put_url)
         print(f"{res.status_code=} {res.reason=}")
         if res.status_code != 200:
-            print(f"ERROR {res.content[:90]}")
+            print(f"### ERROR {res.content[:90]}")
         os.environ["AWS_PROFILE"] = aws_profile
         print()
